ceebcf50697c7ada92fe19d36a1485733ba97198
KeineKohleDevf <keinekohledevf@gmail.com>
2022-10-25 15:01:30 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    // Main methode
+    public static void main(String[] args)
+    {
+        //Prints 'Dear Prudence' to the console
+        System.out.println("Dear Prudence");
+    }
+}
